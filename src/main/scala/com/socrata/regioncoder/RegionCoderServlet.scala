070921311423e1dd103009f29e11bf259d892022
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2015-04-30 08:45:49 -0700

    Empty Scalatra template

---
+++ src/main/scala/com/socrata/regioncoder/RegionCoderServlet.scala
@@ -0,0 +1,17 @@
+package com.socrata.regioncoder
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class RegionCoderServlet extends RegionCoderStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+  
+}
---
+++ src/test/scala/com/socrata/regioncoder/RegionCoderServletSpec.scala
@@ -0,0 +1,16 @@
+package com.socrata.regioncoder
+
+import org.scalatra.test.specs2._
+
+// For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
+class RegionCoderServletSpec extends ScalatraSpec { def is =
+  "GET / on RegionCoderServlet"                     ^
+    "should return status 200"                  ! root200^
+                                                end
+
+  addServlet(classOf[RegionCoderServlet], "/*")
+
+  def root200 = get("/") {
+    status must_== 200
+  }
+}
