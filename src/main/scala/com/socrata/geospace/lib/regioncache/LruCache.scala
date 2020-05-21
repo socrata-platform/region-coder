ade23e622369e681e884b3c1ff6ceb3098139d69
Robert J. Macomber <robert.macomber@socrata.com>
2020-05-21 10:40:15 -0700

    More synchronization in the LRU cache
    In particular, if two things try to look up two values for the same
    key, ensure it only gets computed once.

--- src/main/scala/com/socrata/geospace/lib/regioncache/LruCache.scala
+++ src/main/scala/com/socrata/geospace/lib/regioncache/LruCache.scala
@@ -1,19 +1,57 @@
 package com.socrata.geospace.lib.regioncache
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable
+
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
 import org.apache.commons.collections4.map.LRUMap
 
 class LruCache[K, V](maxEntries: Int) {
+  private class Waiter {
+    var count = 1
+  }
+
   private val cache = new LRUMap[K, V](maxEntries)
+  private val waiters = new mutable.HashMap[K, Waiter]
 
   def apply(key: K)(f: => V): V = {
     Option(cache.synchronized { cache.get(key) }) match {
       case Some(value) =>
         value
+
       case None =>
-        val value: V = f
-        cache.synchronized(cache.put(key, value))
-        value
+        // it wasn't there; we'll want to compute it, but only if no
+        // one else is trying to compute it at the same time.  We'll
+        // create a lock-per-key to adjudicate that.
+        val waiter =
+          waiters.synchronized {
+            val w = waiters.getOrElseUpdate(key, new Waiter)
+            w.count += 1
+            w
+          }
+
+        try {
+          waiter.synchronized {
+            Option(cache.synchronized { cache.get(key) }) match {
+              case Some(value) =>
+                // someone else got there first
+                value
+
+              case None =>
+                // no one got there first, so finally we actually
+                // compute the value to cache.
+                val value: V = f
+                cache.synchronized { cache.put(key, value) }
+                value
+            }
+          }
+        } finally {
+          waiters.synchronized {
+            waiter.count -= 1
+            if(waiter.count == 0) waiters.remove(key)
+          }
+        }
     }
   }
 
