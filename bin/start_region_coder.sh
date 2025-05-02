903a589484882b896c8cabfaa53d1f085ad4537b
sarah-salinas <73255315+sarah-salinas@users.noreply.github.com>
2025-05-02 11:10:05 -0400

    EN-77317: update pipelines version (#45)
    * EN-77317: update pipelines version
    
    * Update bin/start_region_coder.sh for the new name
    
    ---------
    
    Co-authored-by: Robert J. Macomber <robert.macomber@socrata.com>

--- bin/start_region_coder.sh
+++ bin/start_region_coder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Start region-coder
 BASEDIR=$(dirname $0)/..
-JARFILE=$BASEDIR/target/scala-2.10/region-coder-assembly-*.jar
+JARFILE=$BASEDIR/target/scala-2.10/region-coder-assembly.jar
 if [ ! -e $JARFILE ]; then
   cd $BASEDIR && sbt clean assembly
 fi
